8a89d6be83dc74d7e4d6cf7299b9ccebe6ec495a
mondayCoding <moriluxuria@gmail.com>
2018-10-13 00:15:41 +0300

    new styles separated to parths

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import * as ReactDOM from 'react-dom';
 import App from './Layout/Site';
 
 // Import internal styles
-import './Styles/style.css';
+import './Styles/universal.scss';
+import './Styles/animation.scss';
+import './Styles/utilities.scss';
+import './Styles/style.scss';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
